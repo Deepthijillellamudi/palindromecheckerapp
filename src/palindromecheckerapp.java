86611ae2ed800b69bf4b9c748cc0ecc452014dee
Deepthijillellamudi <dj7893@srmist.edu.in>
2026-03-02 16:16:52 +0530

    UC7: Deque-Based Optimized Palindrome Checker

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,32 +1,29 @@
-import java.util.Stack;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
 class PalindromeCheckerApp {
 
    public static void main(String[] args) {
 
       // Given string
-      String word = "level";
+      String word = "radar";
 
-      // Create Stack (LIFO)
-      Stack<Character> stack = new Stack<>();
+      // Create Deque
+      Deque<Character> deque = new ArrayDeque<>();
 
-      // Create Queue (FIFO)
-      Queue<Character> queue = new LinkedList<>();
-
-      // Enqueue and Push characters
+      // Insert characters into deque
       for (int i = 0; i < word.length(); i++) {
-         char ch = word.charAt(i);
-         stack.push(ch);     // LIFO
-         queue.add(ch);      // FIFO (Enqueue)
+         deque.addLast(word.charAt(i));   // insert at rear
       }
 
       boolean isPalindrome = true;
 
-      // Compare dequeue vs pop
-      while (!stack.isEmpty()) {
-         if (stack.pop() != queue.remove()) { // remove() = Dequeue
+      // Compare front and rear
+      while (deque.size() > 1) {
+         char front = deque.removeFirst();  // remove from front
+         char rear = deque.removeLast();    // remove from rear
+
+         if (front != rear) {
             isPalindrome = false;
             break;
          }
