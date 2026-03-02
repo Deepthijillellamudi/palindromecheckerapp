5ae77f455dbb112d7d88fc8e5f497b34265012f8
Deepthijillellamudi <dj7893@srmist.edu.in>
2026-03-02 15:20:09 +0530

    UC1: Add welcome message module

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,2 +1,14 @@
 public class palindromecheckerapp {
+
+ public static void main(String[] args) {
+
+    System.out.println("=================================");
+    System.out.println("   Welcome to PalindromeChecker  ");
+    System.out.println("=================================");
+    System.out.println("Application Version: 1.0");
+    System.out.println("Developed for learning OOPS concepts.");
+    System.out.println("---------------------------------");
+
+    // Program can now continue to next use case
+}
 }
